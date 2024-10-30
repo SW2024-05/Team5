a03b9b97c1f836f630c324e8de25e16ee84f49ea
Ubuntu <ubuntu@ip-172-31-71-109.ec2.internal>
2024-10-30 08:01:32 +0000

    Renderの設定を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+   config.hosts << "ea759f53af934defbbc049c5379d2bb9.vfs.cloud9.us-east-1.amazonaws.com"
 end

cfff11a1aa37f38c21b6978ba4be6e50835cd597
natsu727 <himajin0717@gmail.com>
2024-11-20 09:04:45 +0000

    採寸データの参照・入力実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'fittings/new', to: 'fittings#new'
-  post 'fittings', to: 'fittings#create'
+  # get 'fittings/new', to: 'fittings#new'
+  # post 'fittings', to: 'fittings#create'
+  get "fittings/:id/edit" , to: "fittings#edit"
+  patch 'fittings/:id', to: "fittings#update", as: "fitting"
   # root 'fittings#new'
   # get 'students/index'
   root'students#index'

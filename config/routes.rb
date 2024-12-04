590facf7b9bed1e6de5644048860d97a2bed60be
Hina Isakai <2211140040e@ed.fuk.kindai.ac.jp>
2024-12-04 06:20:28 +0000

    学生情報画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  # get 'fittings/new', to: 'fittings#new'
-  # post 'fittings', to: 'fittings#create'
   get "fittings/:id/edit" , to: "fittings#edit"
   patch 'fittings/:id', to: "fittings#update", as: "fitting"
-  # root 'fittings#new'
-  # get 'students/index'
-  root'students#index'
-  # get 'students/show'
+
   resources :students
+
+  get 'top/show'
+  post 'top/log'
+  root 'top#show'
 
 end
